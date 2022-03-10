e326b80755d6bc562217552aca14b5cc3b8bb757
Sudhakar <bssr93@gmail.com>
2022-03-10 22:36:16 +0530

    first commit

---
+++ src/main/java/com/AddServlet.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int i = Integer.parseInt(request.getParameter("num1"));
+		int j = Integer.parseInt(request.getParameter("num2"));
+		int k = i+j;
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Addition of "+i+" and "+ j+" "
+				+ " is " +k);
+		}
+
+}
